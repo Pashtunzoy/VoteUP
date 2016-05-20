48771bb3dad1f3c05ff939c1ad831cf244c1aa71
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-05-20 20:54:57 +1000

    Added some dependencies(WIP)

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
